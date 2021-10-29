39b63a104c8f3c32ae5e5caa736dd40cdfeb9421
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-10-29 16:31:55 -0500

    Inicio del proyecto

---
+++ src/main/java/com/edflor/springboot/error/app/SpringBootHorarioErrorApplication.java
@@ -0,0 +1,13 @@
+package com.edflor.springboot.error.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHorarioErrorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHorarioErrorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edflor/springboot/error/app/SpringBootHorarioErrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edflor.springboot.error.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHorarioErrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
